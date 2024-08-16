966286d23d1d564163d31769b0538994187602ec
Ashik Ahammed Hridoy <35147425+ahammedhridoy@users.noreply.github.com>
2024-08-16 16:36:58 +0600

    UI Changes done

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className="footer h-[100px] bg-[#26561A] text-white text-center flex justify-center items-center">
+      <p className="text-[16px] font-bold">
+        Copyright © 2024 Tropcey. All Rights Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
